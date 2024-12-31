129bea422608fe9c33734a69505e391bc4f7296f
faizan ali <faizanakmal207@gmail.com>
2025-01-01 00:32:18 +0500

    adding files

---
+++ function.py
@@ -0,0 +1,9 @@
+#possional arguments
+def sum(n1,n2):
+    return n1+n2
+x = int(input("Enter first no:"))
+y = int(input("Enter your 2nd no:"))
+print("Your ans is :",sum(x,y))
+
+#KEY word arguments
+#print("Your ans is :",sum(n2=x,n1=y))
